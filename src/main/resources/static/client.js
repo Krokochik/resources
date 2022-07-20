cba223584e695e2911c595a045d6362aa5a3dd62
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-07-21 01:09:06 +0300

    Hotfix

--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -1,39 +1,39 @@
 var requestUrl = 'https://random--number.herokuapp.com/post/3~01YZH>Iysc8~e><A8x'
 
 function sendRequest(method, url, body = null) {
-  const headers = {
-    'Content-Type': 'application/json'
-  }
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
 
-  return fetch(url, {
-    method: method,
-    body: JSON.stringify(body),
-    headers: headers
-  }).then(response => {
-    if (response.ok) {
-      return response.json()
+    xhr.open(method, url)
+
+    xhr.responseType = 'json'
+    xhr.setRequestHeader('Content-Type', 'application/json')
+
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response)
+      } else {
+        resolve(xhr.response)
+      }
     }
 
-    return response.json().then(error => {
-      const e = new Error('Что-то пошло не так')
-      e.data = error
-      throw e
-    })
+    xhr.onerror = () => {
+      reject(xhr.response)
+    }
+
+    xhr.send(JSON.stringify(body))
   })
 }
 
-/*
-sendRequest('GET', requestUrl)
-  .then(data => console.log(data))
-  .catch(err => console.log(err))
-*/
-///*
+//sendRequest('GET', requestUrl)
+//  .then(data => console.log(data))
+//  .catch(err => console.log(err))
+
 const body = {
-  name: 'Pop',
-  age: 72
+  name: 'Vladilen',
+  age: 26
 }
 
 sendRequest('POST', requestUrl, body)
   .then(data => console.log(data))
   .catch(err => console.log(err))
-//*/
