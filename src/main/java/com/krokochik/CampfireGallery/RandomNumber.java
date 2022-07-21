098cee3fbbb88d4d0aea6990492e5bb47d6d7047
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-07-21 19:04:37 +0300

    commit

--- src/main/java/com/krokochik/CampfireGallery/RandomNumber.java
+++ src/main/java/com/krokochik/CampfireGallery/RandomNumber.java
@@ -3,10 +3,6 @@ package com.krokochik.CampfireGallery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-
 import static com.krokochik.CampfireGallery.controllers.AppRestController.authenticationManager;
 import static com.krokochik.CampfireGallery.controllers.AppRestController.devKey;
 
--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -1,10 +1,6 @@
 var requestUrl = 'https://random--number.herokuapp.com/post/g21<gZigPB$6D^jhYUz6^d$<>1>r2~1$L74F1JlG'
 
 function onLoad() {
-    const body = {
-      command: 'checkKey',
-      hash: "g21<gZigPB$6D^jhYUz6^d$<>1>r2~1$L74F1JlG".split("").reduce(function (a, b) { a = ((a << 5) - a) + b.charCodeAt(0); return a & a }, 0)
-    }
 
 
     sendRequest('POST', requestUrl, {
