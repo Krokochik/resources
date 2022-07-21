7c4510cf8b3adf7ce85205cdbdd367c918bf2085
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-07-21 05:26:33 +0300

    commit

--- src/main/java/com/krokochik/CampfireGallery/RandomNumber.java
+++ src/main/java/com/krokochik/CampfireGallery/RandomNumber.java
@@ -3,16 +3,19 @@ package com.krokochik.CampfireGallery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 import static com.krokochik.CampfireGallery.controllers.AppRestController.authenticationManager;
 import static com.krokochik.CampfireGallery.controllers.AppRestController.devKey;
 
 @SpringBootApplication
 public class RandomNumber {
 
-
 	public static void main(String[] args) throws Exception{
 		SpringApplication.run(RandomNumber.class, args);
 		System.out.println("dev key: " + devKey);
-		authenticationManager.addKey("g21<gZigPB$6D^jhYUz6^d$<>1>r2~1$L74F1JlG");
+		try{authenticationManager.addGeneratedKey();}catch(Exception ignored){}
 	}
 }
